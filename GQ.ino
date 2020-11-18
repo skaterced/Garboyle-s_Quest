04f7aed48f9f818640ca10b34738034ad58861d4
skaterced <61429966+skaterced@users.noreply.github.com>
2020-11-18 17:10:10 +0100

    more sprites

---
+++ GQ.ino
@@ -0,0 +1,69 @@
+/*
+  Mystic Balloon: http://www.team-arg.org/mybl-manual.html
+
+  Arduboy version 1.7.2:  http://www.team-arg.org/mybl-downloads.html
+
+  MADE by TEAM a.r.g. : http://www.team-arg.org/more-about.html
+
+  2016-2018 - GAVENO - CastPixel - JO3RI - Martian220
+
+  Game License: MIT : https://opensource.org/licenses/MIT
+
+
+After I removed the audio:
+Sketch uses 26780 bytes (93%) of program storage space. Maximum is 28672 bytes.
+Global variables use 1653 bytes (64%) of dynamic memory, leaving 907 bytes for local variables. Maximum is 2560 bytes.
+
+
+
+
+
+*/
+
+//determine the game
+#define GAME_ID 34
+
+#include "globals.h"
+#include "menu.h"
+#include "game.h"
+#include "inputs.h"
+#include "player.h"
+#include "enemies.h"
+#include "elements.h"
+#include "levels.h"
+
+
+typedef void (*FunctionPointer) ();
+
+const FunctionPointer PROGMEM  mainGameLoop[] = {
+  stateMenuIntro,
+  stateMenuMain,
+  stateMenuHelp,
+  stateMenuPlaySelect,
+  stateMenuInfo,
+  stateMenuSoundfx,
+  stateGameNextLevel,
+  stateGamePlaying,
+  stateGamePause,
+  stateGameOver,
+  stateMenuPlayContinue,
+  stateMenuPlayNew,
+};
+
+void setup()
+{
+  arduboy.boot();                                           // begin with the boot logo en setting up the device to work
+  //arduboy.audio.begin();
+  arduboy.bootLogoSpritesSelfMasked();
+  arduboy.setFrameRate(60);                                 // set the frame rate of the game at 60 fps
+  loadSetEEPROM();
+}
+
+void loop() {
+  if (!(arduboy.nextFrame())) return;
+  if (gameState < STATE_GAME_NEXT_LEVEL && arduboy.everyXFrames(10))sparkleFrames = (++sparkleFrames) % 5;
+  arduboy.pollButtons();
+  arduboy.clear();
+  ((FunctionPointer) pgm_read_word (&mainGameLoop[gameState]))();
+  arduboy.display();
+}
